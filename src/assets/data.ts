b8cf58c480ee893a9d22b22663c79cd987203463
dumani <dumanimedia@gmail.com>
2023-09-09 18:06:06 +0300

    deploy to vercel

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -29,49 +29,46 @@ export const servicesInfo: Service[] = [
   {
     name: 'logistics',
     subtitle:
-      'Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas',
+      "We'll help manage the flow of goods and services from the point of origin to the point of consumption.",
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias deleniti aperiam qui, magni iste sapiente culpa totam quia corrupti modi incidunt voluptate, non temporibus impedit repudiandae repellendus, sequi repellat quisquam. Harum, quam? Rem libero labore asperiores? Mollitia consectetur delectus ad consequuntur, iusto labore. Officiis dolores eligendi in cupiditate architecto sit.',
+      'When you use a logistics provider, you are essentially giving them control of your goods. This can be a challenge if you are not familiar with the provider or if you have had bad experiences with other providers in the past. It is important to communicate your needs clearly to the logistics provider so that they can provide the best possible service',
     image: logisticsService,
     offers: [
-      'Aut eum totam accusantium voluptatem.',
-      'Assumenda et porro nisi nihil nesciunt voluptatibus.',
+      "We' help negotiate better rates with carriers and other suppliers.",
+      "We'll help your business to streamline their supply chain operations.",
     ],
     otherInfo: [
-      'Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste veniam repellat odit. Aut pariatur itaque nesciunt fuga.',
-      'Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum. Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.',
+      'It is important to set realistic expectations with the logistics provider so that you are not disappointed with the results.',
+      'Overall, logistics services can be a valuable asset for businesses of all sizes. By carefully selecting a provider and managing expectations, businesses can reap the many benefits of using logistics services.',
     ],
   },
   {
     name: 'storage',
     subtitle:
-      'Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas',
+      'How much data do you need to store? How often do you need to access the data? How important is security?',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias deleniti aperiam qui, magni iste sapiente culpa totam quia corrupti modi incidunt voluptate, non temporibus impedit repudiandae repellendus, sequi repellat quisquam. Harum, quam? Rem libero labore asperiores? Mollitia consectetur delectus ad consequuntur, iusto labore. Officiis dolores eligendi in cupiditate architecto sit.',
+      "Once you've considered these factors, you can start brainstorming specific information about the storage service you're looking for. Here are some questions you can ask yourself. Here are some of the most popular storage services:",
     image: storageService,
-    offers: [
-      'Aut eum totam accusantium voluptatem.',
-      'Assumenda et porro nisi nihil nesciunt voluptatibus.',
-    ],
+    offers: ['Object storage', 'Block storage'],
     otherInfo: [
-      'Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste veniam repellat odit. Aut pariatur itaque nesciunt fuga.',
-      'Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum. Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.',
+      'Object storage: Object storage is a type of storage that stores data as objects. Objects are typically made up of a key, value, and metadata. This type of storage is often used for storing large amounts of unstructured data. Some popular object storage providers include Amazon S3, Google Cloud Storage, and IBM Cloud Object Storage.',
+      'Block storage: Block storage is a type of storage that stores data as blocks. Blocks are typically 512 bytes or 4 kilobytes in size. This type of storage is often used for storing virtual machines and databases. Some popular block storage providers include Amazon EBS, Google Compute Engine Persistent Disk, and Microsoft Azure Managed Disks.',
     ],
   },
   {
     name: 'cargo',
     subtitle:
-      'Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas',
+      'Cargo services are a transportation service that is used to transport large quantities of goods over long distances.',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias deleniti aperiam qui, magni iste sapiente culpa totam quia corrupti modi incidunt voluptate, non temporibus impedit repudiandae repellendus, sequi repellat quisquam. Harum, quam? Rem libero labore asperiores? Mollitia consectetur delectus ad consequuntur, iusto labore. Officiis dolores eligendi in cupiditate architecto sit.',
+      'They are typically used by businesses to transport raw materials, finished goods, and other products. Cargo services can be arranged by air, sea, or land. Cargo services include;',
     image: cargoService,
-    offers: [
-      'Aut eum totam accusantium voluptatem.',
-      'Assumenda et porro nisi nihil nesciunt voluptatibus.',
-    ],
+    offers: ['Door-to-door Cargo service.', 'Port-to-port Cargo service.'],
     otherInfo: [
-      'Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste veniam repellat odit. Aut pariatur itaque nesciunt fuga.',
-      'Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum. Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.',
+      "Door-to-door services pick up the goods from the shipper's location and deliver them to the recipient's location.",
+      'Port-to-port services only transport the goods between ports. The cost of cargo services will vary depending on the mode of transportation, the distance traveled, and the weight and size of the goods being shipped.',
+      'Cargo services help businesses to save time and money by transporting large quantities of goods in bulk.',
+      'Cargo services help businesses to reach a wider range of customers by shipping goods to different parts of the world.',
+      'Cargo services help businesses to reduce their carbon footprint by transporting goods in a more efficient way.',
     ],
   },
   {
@@ -109,17 +106,15 @@ export const servicesInfo: Service[] = [
   {
     name: 'warehousing',
     subtitle:
-      'Temporibus et in vero dicta aut eius lidero plastis trand lined voluptas dolorem ut voluptas',
+      'Warehousing is the process of storing physical inventory for sale or distribution.',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias deleniti aperiam qui, magni iste sapiente culpa totam quia corrupti modi incidunt voluptate, non temporibus impedit repudiandae repellendus, sequi repellat quisquam. Harum, quam? Rem libero labore asperiores? Mollitia consectetur delectus ad consequuntur, iusto labore. Officiis dolores eligendi in cupiditate architecto sit.',
+      ' It helps businesses to improve efficiency, reduce costs, and meet customer demands.There are many different types of warehousing services available, each with its own set of benefits and drawbacks. Some of the most common types of warehousing services include:',
     image: warehousingService,
-    offers: [
-      'Aut eum totam accusantium voluptatem.',
-      'Assumenda et porro nisi nihil nesciunt voluptatibus.',
-    ],
+    offers: ['Public warehouses', 'Private warehouses.', 'Contract warehouses'],
     otherInfo: [
-      'Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste veniam repellat odit. Aut pariatur itaque nesciunt fuga.',
-      'Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum. Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.',
+      'Public warehouses: These are warehouses that are owned and operated by a third-party company. They offer a variety of services, including storage, picking, packing, and shipping.',
+      'Private warehouses: These are warehouses that are owned and operated by the business itself. They offer more control over the storage and handling of goods, but they can also be more expensive.',
+      'Contract warehouses: These are warehouses that are leased to businesses for a specific period of time. They offer a flexible option for businesses that need temporary storage space. When choosing a warehousing service, businesses need to consider a number of factors, including the type of goods they need to store, the volume of goods, the desired level of service, and the budget. Here are some of the benefits of using warehousing services:',
     ],
   },
 ];

c71b4067b878d0e9950e1239800a1f633893a5a3
dumani <dumanimedia@gmail.com>
2023-09-10 16:58:45 +0300

    remove preline css and add astro seo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/**/*.astro",
-    "./src/**/*.{astro,js,jsx,ts,tsx,vue}",
-    "node_modules/preline/dist/*.js",
-  ],
+  content: ['./public/**/*.astro', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [require("preline/plugin")],
+  plugins: [],
 };
